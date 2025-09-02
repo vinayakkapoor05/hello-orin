ad246c66d26c197b050b07370f7e2887d0a6ced4
Vinayak Kapoor <72437558+vinayakkapoor05@users.noreply.github.com>
2025-09-02 16:11:27 -0500

    publish

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from waggle.plugin import Plugin
 def main():
     with Plugin() as plugin:
         print("This is the start of an amazing app!")
+        plugin.publish("Hellow World")
 
 
 if __name__ == "__main__":
